c61a9820d5f668362a8a305aec9c0df10bf54146
Eric Jacobsen <eric@whiskyvangoghgo.com>
2024-12-24 00:16:55 -0500

    cleanup

--- packages/bluesky-covid-wastewater/post/src/index.ts
+++ packages/bluesky-covid-wastewater/post/src/index.ts
@@ -1,20 +1,9 @@
 import { AtpAgent } from "@atproto/api";
-import { type CdcDatum } from "./types.js";
 import { generateMessage } from "./generateMessage.js";
+import { getData } from "./getData.js";
 
 export const main = async () => {
-  const JSON_URL =
-    "https://www.cdc.gov/wcms/vizdata/NCEZID_DIDRI/NWSSStateMap.json";
-
-  const dataRaw = await fetch(JSON_URL);
-  if (!dataRaw) {
-    throw new Error("Error retrieving data from CDC.");
-  }
-  const dataText = await dataRaw.text();
-  const data = JSON.parse(dataText) as Array<CdcDatum>;
-  if (!data) {
-    throw new Error("Error parsing data JSON.");
-  }
+  const data = await getData();
 
   const text = generateMessage(data);
 
